8d1538326f596235f58b6305b4808c124f38019f
harsha4104 <harshvardhansingh2397@gmail.com>
2024-04-06 17:31:49 +0530

    First commit

---
+++ DesignPatterns/src/designPatterns/creational/SingleTon.java
@@ -0,0 +1,34 @@
+package designPatterns.creational;
+
+public class SingleTon {
+
+    private static SingleTon instance;
+
+    public static SingleTon getInstance(){
+        if(instance == null) {
+            synchronized (SingleTon.class) {
+                // double locking is only useful when the piece of code is inside synchronised block
+                // only one thread will access this block at a time if one thread already created the instance other thread will
+                // be blocked by the inner if check. Also, the thread which don't executed the first will be directly go down
+                // and returned the instance.
+                if (instance == null) {
+                    instance = new SingleTon();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    // other way to do the same thing. Just make the method synchronized
+    public static synchronized SingleTon getInstanceSynchronized(){
+        if(instance == null){
+            instance = new SingleTon();
+        }
+        return instance;
+    }
+
+    private SingleTon(){
+
+    }
+}
