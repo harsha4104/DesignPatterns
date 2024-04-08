aa8b0502d2a16a8347d0dc5ca3d0368b54e43c54
harsha4104 <harshvardhansingh2397@gmail.com>
2024-04-08 22:31:50 +0530

    State design pattern added

---
+++ DesignPatterns/src/designPatterns/behavioural/state/Context.java
@@ -0,0 +1,18 @@
+package designPatterns.behavioural.state;
+
+public class Context {
+
+    private State state;
+
+    public Context(){
+        state = null;
+    }
+
+    public void setState(State state){
+        this.state = state;
+    }
+
+    public State getState(){
+        return this.state;
+    }
+}
---
+++ DesignPatterns/src/designPatterns/behavioural/state/State.java
@@ -0,0 +1,6 @@
+package designPatterns.behavioural.state;
+
+public interface State {
+
+    void doAction(Context context);
+}
